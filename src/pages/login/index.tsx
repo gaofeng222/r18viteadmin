79868f51457bcbbbad125c7a4968f71ba9c2c022
gaofeng222 <596642721@qq.com>
2024-07-05 16:50:01 +0800

    add:晚上登录页面静态开发

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,16 +1,70 @@
-import { useEffect } from "react";
-import star from  '@/utils/star-bg'
+import { Button, Checkbox, Form, Input } from 'antd';
 import style from './login.module.scss'
+import type { FormProps } from 'antd';
+import Logo from '@/assets/images/login-bg.png'
+
+type FieldType = {
+  username?: string;
+  password?: string;
+  remember?: string;
+};
+
 
 function Login(){
-  useEffect(() => {
-    console.log('login')
-    document.title = "Login";
-    star('star')
-  });
+  const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
+    console.log('Success:', values);
+  };
+  
+  const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
   return (
     <div className={style['login-page']}>
-      <canvas id="star" className={style.star}>测试文字</canvas>
+       {/* antd的表单 */}
+      <div className={style['login-page__main']}>
+        <div className={style['left-content']}>
+           <img src={Logo} />
+        </div>
+        <div className={style['right-content']}>
+          <p className={style['login-form-title']}>
+             欢迎使用<span>Viteam</span>管理平台
+          </p>
+          <h2>登录</h2>
+            <Form
+                  labelCol={{ span: 3 }}
+                  wrapperCol={{ span: 18 }}
+                  style={{ maxWidth: 800,minWidth:600}}
+                  initialValues={{ remember: true }}
+                  onFinish={onFinish}
+                  onFinishFailed={onFinishFailed}
+                  autoComplete="off"
+            >
+                <Form.Item 
+                  label="用户名"
+                  name="username"
+                  
+                  rules={[{ required: true, message: '请输入用户名!' }]}>
+                    <Input placeholder="请输入用户名" style={{height:'46px'}} />
+                </Form.Item>
+                <Form.Item 
+                  label="密码"
+                  name="password"
+                    rules={[{ required: true, message: '请输入密码!' }]}>
+                  <Input placeholder="请输入密码" type='password' style={{height:'46px'}} />
+                </Form.Item>
+                <Form.Item<FieldType>
+                  name="remember"
+                  valuePropName="checked"
+                  wrapperCol={{ offset: 2, span: 16 }}
+                >
+                  <Checkbox>Remember me</Checkbox>
+                </Form.Item>
+                <Form.Item  wrapperCol={{ offset: 2, span: 19 }}>
+                    <Button style={{width:"100%",height:'46px',fontSize:"20px"}} type="primary" htmlType="submit">登录</Button>
+                </Form.Item>
+            </Form>
+            </div>
+        </div>
     </div>
   )
 }
